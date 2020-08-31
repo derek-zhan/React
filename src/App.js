c2e028793b7b39ecc2eefe2dec511949677056cd
Derek <derek-zhan@users.noreply.github.com>
2020-08-31 09:52:56 -0400

    Roll back to class-based implementation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-const app = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29}
     ]
-  });
+  }
 
-  const [otherState, setOtherState] = useState('some other value');
-
-  const switchNameHandler = () => {
-    setPersonsState({
+  switchNameHandler = () => {
+    this.setState({
       persons: [
         {name: 'Maximilian', age: 28},
         {name: 'Manu', age: 29}
@@ -21,17 +19,19 @@ const app = props => {
     })
   }
 
+  render() {
     return (
       <div className="App">
         <h1> Hi, I am React App</h1>
         <p>This is really working</p>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
 
 
       </div>
     );
+  }
 }
 
-export default app;
+export default App;
