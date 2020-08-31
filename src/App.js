89124f05c5482f787e16bc21ae6a2327b8e2ef14
Derek <derek-zhan@users.noreply.github.com>
2020-08-31 09:44:19 -0400

    Using React Hooks
    useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29}
-    ]
-  }
+    ],
+    otherState:'blah'
+  });
 
-  switchNameHandler = () => {
+  const switchNameHandler = () => {
     this.setState({
       persons: [
         {name: 'Maximilian', age: 28},
@@ -19,19 +20,17 @@ class App extends Component {
     })
   }
 
-  render() {
     return (
       <div className="App">
         <h1> Hi, I am React App</h1>
         <p>This is really working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
 
 
       </div>
     );
-  }
 }
 
-export default App;
+export default app;
